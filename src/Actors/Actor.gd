ee6747c9119da9a6c33f1fa2ebdf93a3f6b52316
Fahim <fa97@hw.ac.uk>
2020-11-01 20:50:15 +0400

    2D PLatformer

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP;
+
+var _velocity: = Vector2.ZERO;
+export var gravity: = 4000.0;
+export var speed: = Vector2(300.0, 1000.0);
---
+++ src/Scene/Panel.gd
@@ -0,0 +1,7 @@
+extends Panel
+
+func _ready():
+	pass
+
+func _on_Button_pressed():
+	$Label.text = "Hello World!";
